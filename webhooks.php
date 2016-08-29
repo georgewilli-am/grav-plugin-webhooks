0f884fb3d33d8a9573f6986efd3b948e4bfd8457
George Sumpster <gsumpster@gmail.com>
2016-08-29 01:22:16 -0700

    added webhook autoloading & hook interface

--- webhooks.php
+++ webhooks.php
@@ -10,16 +10,8 @@ use RocketTheme\Toolbox\Event\Event;
  */
 class WebhooksPlugin extends Plugin
 {
-    /**
-     * @return array
-     *
-     * The getSubscribedEvents() gives the core a list of events
-     *     that the plugin wants to listen to. The key of each
-     *     array section is the event that the plugin listens to
-     *     and the value (in the form of an array) contains the
-     *     callable (or function) as well as the priority. The
-     *     higher the number the higher the priority.
-     */
+    private HookClasses = array();
+
     public static function getSubscribedEvents()
     {
         return [
@@ -32,33 +24,35 @@ class WebhooksPlugin extends Plugin
      */
     public function onPluginsInitialized()
     {
-        // Don't proceed if we are in the admin plugin
-        if ($this->isAdmin()) {
-            return;
+        if($this->config->get('plugins.webhooks.indiviual_hooks')){
+          for($this->config->get('plugins.webhooks.hooks' as $hook => $url)){
+            if($url != null || $url != false){
+              require(__DIR__ . '/hooks/' $key . '.php');
+              $hookObj = new $hook();
+              $hookObj->init($url);
+              HookClasses[] = $hookObj;
+            }
+          }
+        }else{
+          for($this->config->get('plugins.webhooks.hooks') as $hook => $url){
+            if($url != false){
+              require(__DIR__ . '/hooks/' $key . '.php');
+              $hookObj = new $hook();
+              $hookObj->init($this->config->get('plugins.webhooks.webhook_url(s)'));
+              HookClasses[] = $hookObj;
+            }
+          }
         }
 
-        // Enable the main event we are interested in
         $this->enable([
-            'onPageContentRaw' => ['onPageContentRaw', 0]
+            'onShutdown' => ['onShutdown', 0]
         ]);
     }
 
-    /**
-     * Do some work for this event, full details of events can be found
-     * on the learn site: http://learn.getgrav.org/plugins/event-hooks
-     *
-     * @param Event $e
-     */
-    public function onPageContentRaw(Event $e)
-    {
-        // Get a variable from the plugin configuration
-        $text = $this->grav['config']->get('plugins.webhooks.text_var');
-
-        // Get the current raw content
-        $content = $e['page']->getRawContent();
-
-        // Prepend the output with the custom text and set back on the page
-        $e['page']->setRawContent($text . "\n\n" . $content);
+    public function onShutdown(){
+      for($HookClasses as $hook){
+        $hook.process();
+      }
     }
 
 }
